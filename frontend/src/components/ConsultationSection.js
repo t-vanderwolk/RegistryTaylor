3196b86fa81fccb6846c3541e7d22a3c7a8c1866
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2025-09-19 18:51:03 -0700

    embedded trafft

--- frontend/src/components/ConsultationSection.js
+++ frontend/src/components/ConsultationSection.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 
 const ConsultationSection = () => {
   useEffect(() => {
+    // Ensure script runs after component mounts
     const script = document.createElement("script");
     script.src = "https://babyconcierge.totsquad.com/embed.js";
     script.async = true;
@@ -23,7 +24,7 @@ const ConsultationSection = () => {
         data-query="&t=s&uuid=84602e01-c352-4443-a7c8-a8d27e9e207c"
         data-employee="taylor-vanderwolk"
         data-lang="en"
-        data-autoresize="0"
+        data-autoresize="1"
         data-showsidebar="1"
         data-showservices="0"
         style={{ minWidth: "320px", height: "768px" }}
